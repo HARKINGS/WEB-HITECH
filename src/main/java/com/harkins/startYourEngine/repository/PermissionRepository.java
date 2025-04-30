3fecf041af4b74916d2e7a618752f7f5370c1d93
Nguyen Huy Nam <huynambka@gmail.com>
2025-04-30 15:40:49 +0700

    refactor

--- src/main/java/com/harkins/startYourEngine/repository/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.harkins.startYourEngine.repository;
-
-import com.harkins.startYourEngine.entity.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PermissionRepository extends JpaRepository<Permission, String> {
-}
