65f017d9c3a8d1c461ccc125e2080b083e460ab0
Nguyen Huy Nam <huynambka@gmail.com>
2025-05-18 21:13:18 +0700

    merge be and fe to main

--- src/main/java/com/harkins/startYourEngine/repository/InvalidatedTokenRepository.java
+++
@@ -1,8 +0,0 @@
-package com.harkins.startYourEngine.repository;
-
-import com.harkins.startYourEngine.entity.InvalidatedToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface InvalidatedTokenRepository extends JpaRepository<InvalidatedToken, String> {
-
-}
--- src/main/java/com/harkins/startYourEngine/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.harkins.startYourEngine.repository;
-
-import com.harkins.startYourEngine.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface RoleRepository extends JpaRepository<Role, String> {
-}
