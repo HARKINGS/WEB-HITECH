93de8ba24e4cef3be61256d6aeaf1e76b801c6e8
Harkins <tmngoc1108@gmail.com>
2025-05-18 09:01:48 +0700

    bỏ comment solve exception

--- src/main/java/com/harkins/startYourEngine/exception/GlobalHandleExeption.java
+++ src/main/java/com/harkins/startYourEngine/exception/GlobalHandleExeption.java
@@ -21,69 +21,69 @@ import lombok.extern.slf4j.Slf4j;
 public class GlobalHandleExeption {
     private static final String MIN_ATTRIBUTE = "min";
 
-//    @ExceptionHandler(value = Exception.class)
-//    ResponseEntity<ApiResponse> handleRuntimeException(RuntimeException e) {
-//        ApiResponse apiResponse = new ApiResponse();
-//
-//        ErrorCode errorCode = ErrorCode.UNCATEGORIZED_EXCEPTION;
-//
-//        System.out.println(e.toString());
-//
-//        apiResponse.setCode(errorCode.getCode());
-//        apiResponse.setMessage(errorCode.getMessage());
-//
-//        return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
-//    }
-//
-//    @ExceptionHandler(value = AppException.class)
-//    ResponseEntity<ApiResponse> handleAppException(AppException e) {
-//        ErrorCode errorCode = e.getErrorCode();
-//        ApiResponse apiResponse = new ApiResponse();
-//
-//        apiResponse.setCode(errorCode.getCode());
-//        apiResponse.setMessage(errorCode.getMessage());
-//
-//        return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
-//    }
-//
-//    @ExceptionHandler({AccessDeniedException.class, AuthorizationDeniedException.class})
-//    ResponseEntity<ApiResponse> handleAccessDeniedException(Exception e) {
-//        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
-//        return ResponseEntity.status(errorCode.getStatusCode())
-//                .body(ApiResponse.builder()
-//                        .code(errorCode.getCode())
-//                        .message(errorCode.getMessage())
-//                        .build());
-//    }
-//
-//    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-//    ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-//        String enumKey = e.getFieldError().getDefaultMessage();
-//        ErrorCode errorCode = ErrorCode.INVALID_KEY;
-//
-//        Map<String, Object> attributes = null;
-//
-//        try {
-//            errorCode = ErrorCode.valueOf(enumKey);
-//            var constraintViolations =
-//                    e.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
-//            // Lay ra loi dau tien cos kieu ConstraintViolation
-//            attributes = constraintViolations
-//                    .getConstraintDescriptor() // Lấy ra DobConstraint (annotation)
-//                    .getAttributes(); // Lấy ra tất cả thuộc tính của DobConstraint
-//            log.info(attributes.toString());
-//        } catch (IllegalArgumentException ex) {
-//
-//        }
-//
-//        ApiResponse apiResponse = new ApiResponse();
-//        apiResponse.setCode(errorCode.getCode());
-//        apiResponse.setMessage(
-//                Objects.nonNull(attributes)
-//                        ? mapAttribute(errorCode.getMessage(), attributes)
-//                        : errorCode.getMessage());
-//        return ResponseEntity.badRequest().body(apiResponse);
-//    }
+    @ExceptionHandler(value = Exception.class)
+    ResponseEntity<ApiResponse> handleRuntimeException(RuntimeException e) {
+        ApiResponse apiResponse = new ApiResponse();
+
+        ErrorCode errorCode = ErrorCode.UNCATEGORIZED_EXCEPTION;
+
+        System.out.println(e.toString());
+
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(errorCode.getMessage());
+
+        return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
+    }
+
+    @ExceptionHandler(value = AppException.class)
+    ResponseEntity<ApiResponse> handleAppException(AppException e) {
+        ErrorCode errorCode = e.getErrorCode();
+        ApiResponse apiResponse = new ApiResponse();
+
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(errorCode.getMessage());
+
+        return ResponseEntity.status(errorCode.getStatusCode()).body(apiResponse);
+    }
+
+    @ExceptionHandler({AccessDeniedException.class, AuthorizationDeniedException.class})
+    ResponseEntity<ApiResponse> handleAccessDeniedException(Exception e) {
+        ErrorCode errorCode = ErrorCode.UNAUTHORIZED;
+        return ResponseEntity.status(errorCode.getStatusCode())
+                .body(ApiResponse.builder()
+                        .code(errorCode.getCode())
+                        .message(errorCode.getMessage())
+                        .build());
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    ResponseEntity<ApiResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String enumKey = e.getFieldError().getDefaultMessage();
+        ErrorCode errorCode = ErrorCode.INVALID_KEY;
+
+        Map<String, Object> attributes = null;
+
+        try {
+            errorCode = ErrorCode.valueOf(enumKey);
+            var constraintViolations =
+                    e.getBindingResult().getAllErrors().getFirst().unwrap(ConstraintViolation.class);
+            // Lay ra loi dau tien cos kieu ConstraintViolation
+            attributes = constraintViolations
+                    .getConstraintDescriptor() // Lấy ra DobConstraint (annotation)
+                    .getAttributes(); // Lấy ra tất cả thuộc tính của DobConstraint
+            log.info(attributes.toString());
+        } catch (IllegalArgumentException ex) {
+
+        }
+
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setCode(errorCode.getCode());
+        apiResponse.setMessage(
+                Objects.nonNull(attributes)
+                        ? mapAttribute(errorCode.getMessage(), attributes)
+                        : errorCode.getMessage());
+        return ResponseEntity.badRequest().body(apiResponse);
+    }
 
     private String mapAttribute(String message, Map<String, Object> attributes) {
         String minValue = attributes.get(MIN_ATTRIBUTE).toString();
