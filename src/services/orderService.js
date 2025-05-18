39f9043ac9a59d1842fbe970237fbf176b407b41
Nguyen Huy Nam <huynambka@gmail.com>
2025-05-18 23:44:40 +0700

    update order

--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,7 +1,7 @@
 // src/services/orderService.js
 import axios from 'axios';
 
-const API_URL = `${process.env.REACT_APP_API_BASE_URL}/api/orders`;
+const API_URL = `${process.env.REACT_APP_API_BASE_URL}/orders`;
 
 /**
  * Places a new order.
